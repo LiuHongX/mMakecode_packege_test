08854e77b40c4242e9ad6b7a9909374de6f14f00
zhyt <1628472468@qq.com>
2018-06-07 11:40:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# newSpace
+
+newSpace
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
---
+++ custom.ts
@@ -0,0 +1,43 @@
+
+/**
+ * 自定义图形块
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace my_test {
+    /**
+     * TODO: 在此处描述您的函数
+     */
+    //% block
+    export function nullFun(): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: 在此处描述您的函数
+     * @param value 在此处描述”值“, eg: 5
+     */
+    //% block
+    export function test_return(value: number): number {
+        return 1;
+    }
+}
+
+//% weight=100 color=#0fbc11 icon=""
+namespace newSpace {
+    /**
+        * TODO: 在此处描述您的函数
+        */
+    //% block
+    export function new_nullFun(): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: 在此处描述您的函数
+     * @param value 在此处描述”值“, eg: 5
+     */
+    //% block
+    export function new_test_return(value: number): number {
+        return 1;
+    }
+}
